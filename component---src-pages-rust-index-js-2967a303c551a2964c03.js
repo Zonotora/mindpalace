e29054c61af72b3626f732cd55fbe09981c02d83
Zonotora <zonotora@hotmail.com>
2021-01-01 14:50:33 +0000

    Deploying from fb4a785ace5622c764698b9f514d56a16c89e688

--- component---src-pages-rust-index-js-2967a303c551a2964c03.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"M1U+":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),r=a.n(n),l=a("Wbzz"),u=a("Bl7J"),c=a("vrFN");t.default=function(){var e="/rust",t=["test"],a=[];return r.a.createElement(u.a,null,r.a.createElement(c.a,{title:"Home"}),r.a.createElement("h1",null,"This is a template"),t.map((function(t){return r.a.createElement(l.a,{key:t,to:e+"/"+t},t)})),a.map((function(t){return r.a.createElement(l.a,{key:t,to:e+"/"+t},t)})))}}}]);
-//# sourceMappingURL=component---src-pages-rust-index-js-2967a303c551a2964c03.js.map

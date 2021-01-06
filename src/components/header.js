b7a56352e9c6519480afce8a2fc8d8afc93318f9
Zonotora <zonotora@hotmail.com>
2021-01-06 21:17:41 +0100

    Add button for hide/unhide tree view

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,20 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
+const TreeViewButton = ({ onTreeviewClick }) => {
+  return (
+    <div
+      style={{ cursor: "pointer", position: "absolute", color: "white" }}
+      onClick={onTreeviewClick}
+    >
+      tree
+    </div>
+  );
+};
+
 const BackButton = ({ parent }) => {
   return (
-    <div style={{ position: "absolute", color: "white" }}>
+    <div style={{ position: "absolute", color: "white", left: "40px" }}>
       {/* <svg width="200" height="200">
         <path d="M150 0 L75 200 L225 200 Z" stoke="green" fill="green" />
       </svg> */}
@@ -23,7 +34,7 @@ const BackButton = ({ parent }) => {
 
 const HomeButton = () => {
   return (
-    <div style={{ position: "absolute", color: "white", left: "40px" }}>
+    <div style={{ position: "absolute", color: "white", left: "80px" }}>
       <Link
         to="/"
         style={{
@@ -37,9 +48,10 @@ const HomeButton = () => {
   );
 };
 
-const Header = ({ siteTitle, parent }) => (
+const Header = ({ siteTitle, parent, onTreeViewClick }) => (
   <header>
     <div>
+      <TreeViewButton onTreeviewClick={onTreeViewClick} />
       <BackButton parent={parent} />
       <HomeButton />
       <h1 style={{ margin: 0 }}>

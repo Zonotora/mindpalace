822cf5e3b3a3b0786cfdfe4eff9dcba4736856d8
Zonotora <zonotora@hotmail.com>
2021-09-04 11:56:06 +0200

    Remove prints during file generation

--- plugins/gatsby-remark-mindpalace-draw/index.js
+++ plugins/gatsby-remark-mindpalace-draw/index.js
@@ -4,14 +4,10 @@ const { Parser } = require("./parser");
 
 module.exports = ({ markdownAST }, options) => {
   visit(markdownAST, "code", (node) => {
-    // console.log(node);
-
     const parser = new Parser(node);
     const ast = parser.parse();
-    console.log(ast);
     if (ast) {
       const html = generate(ast);
-      console.log(html);
       node.type = "html";
       node.children = undefined;
       node.value = html;

28096e20a663e58b1526fcf193988d0116a10c57
Zonotora <zonotora@hotmail.com>
2021-11-19 14:22:15 +0100

    Have icon instead of text for copy code

--- src/templates/file.js
+++ src/templates/file.js
@@ -198,36 +198,53 @@ const onTreeViewClick = (e, posX, setPosX, isTabletOrMobile) => {
   }
 };
 
+const addSvgAttribute = (element, attributes) => {
+  for (var key in attributes)
+    element.setAttributeNS(null, key, attributes[key]);
+};
+
 const setupPreCopy = () => {
   const pres = document.getElementsByTagName("pre");
   for (const pre of pres) {
     const copy = document.createElement("div");
-    copy.innerHTML = "Copy";
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    svg.appendChild(path);
+    addSvgAttribute(svg, {
+      stroke: "currentColor",
+      fill: "currentColor",
+      "stroke-width": "0",
+      viewBox: "0 0 1024 1024",
+      height: "28px",
+      width: "100%",
+    });
+    addSvgAttribute(path, {
+      d:
+        "M832 64H296c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496v688c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V96c0-17.7-14.3-32-32-32zM704 192H192c-17.7 0-32 14.3-32 32v530.7c0 8.5 3.4 16.6 9.4 22.6l173.3 173.3c2.2 2.2 4.7 4 7.4 5.5v1.9h4.2c3.5 1.3 7.2 2 11 2H704c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32zM350 856.2L263.9 770H350v86.2zM664 888H414V746c0-22.1-17.9-40-40-40H232V264h432v624z",
+    });
+
     const code = pre.getElementsByTagName("code")[0];
     copy.onclick = (e) => {
       const text = code.textContent;
 
       if (!navigator.clipboard) {
-        copy.innerHTML = "Unable to copy!";
         copy.style.backgroundColor = "#db5454";
         return;
       }
       navigator.clipboard.writeText(text).then(
         () => {
-          copy.innerHTML = "Copied!";
           copy.style.backgroundColor = "#98db54";
         },
         () => {
-          copy.innerHTML = "Unable to copy!";
           copy.style.backgroundColor = "#db5454";
         }
       );
     };
 
     pre.onmouseleave = (e) => {
-      copy.innerHTML = "Copy";
       copy.style.backgroundColor = "#ddd";
     };
+    copy.appendChild(svg);
     pre.appendChild(copy);
   }
 };
